861c4a8e726b331cfdc1c3e2bcac79919797aef3
Adarsh-3325 <csds23013@glbitm.ac.in>
2026-07-24 12:52:37 +0530

    Time: 5 ms (49.48%) | Memory: 46.3 MB (99.36%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -8,10 +8,10 @@ class Solution {
             if(map.containsKey(comp)){
                ans[0] =  map.get(comp);
                 ans[1] = i;
-                return ans;
+            
             }
             map.put(nums[i],i);
         }
-        return new int[]{};
+        return ans;
     }
 }
