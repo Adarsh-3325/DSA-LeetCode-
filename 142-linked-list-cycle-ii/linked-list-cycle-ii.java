106cc237939cc76ed0c46d69aa99bd6f1ffad28d
Adarsh-3325 <csds23013@glbitm.ac.in>
2026-09-05 11:32:38 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (69.97%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -11,34 +11,55 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
+        // ListNode slow = head;
+        // ListNode fast = head;
+        // boolean cycle = false;
+
+        // while(fast != null && fast.next != null){
+        //     slow = slow.next;
+        //     fast = fast.next.next;
+
+        //     if(slow == fast){
+        //         cycle = true;
+        //         break;
+        //     }
+        // }
+
+        // if(cycle == false){
+        //     return null;
+        // }
+
+        // slow = head;
+        // ListNode prev = null;
+        
+
+        // while(slow != fast){
+        //     prev = fast;
+        //     slow = slow.next;
+        //     fast = fast.next;
+            
+        // }
+        // return slow;
+
+        // Approach - 2
+
         ListNode slow = head;
         ListNode fast = head;
-        boolean cycle = false;
 
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
 
             if(slow == fast){
-                cycle = true;
-                break;
+                slow = head;
+
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
             }
         }
-
-        if(cycle == false){
-            return null;
-        }
-
-        slow = head;
-        ListNode prev = null;
-        
-
-        while(slow != fast){
-            prev = fast;
-            slow = slow.next;
-            fast = fast.next;
-            
-        }
-        return slow;
+        return null;
     }
 }
