ec7ade07a5e0a0db18ed1fce19709ab01ceee6aa
Adarsh-3325 <csds23013@glbitm.ac.in>
2026-09-07 07:21:32 +0530

    Time: 19 ms (9.00%) | Memory: 93.5 MB (6.60%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -4,23 +4,36 @@ class Solution {
         int n = nums.length;
 
         
-        for (int i = 0; i < n; i++) {
-            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+        // for (int i = 0; i < n; i++) {
+        //     while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
                 
-                int temp = nums[nums[i] - 1];
-                nums[nums[i] - 1] = nums[i];
-                nums[i] = temp;
-            }
-        }
+        //         int temp = nums[nums[i] - 1];
+        //         nums[nums[i] - 1] = nums[i];
+        //         nums[i] = temp;
+        //     }
+        // }
 
        
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != i + 1) {
-                return i + 1;
-            }
-        }
+        // for (int i = 0; i < n; i++) {
+        //     if (nums[i] != i + 1) {
+        //         return i + 1;
+        //     }
+        // }
 
        
-        return n + 1;
+        // return n + 1;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        }
+
+        int min = Integer.MAX_VALUE;
+        for(int i=1;i<=n+1;i++){
+            if(!map.containsKey(i)){
+                min = Math.min(min,i);
+            }
+        }
+        return min;
     }
 }
